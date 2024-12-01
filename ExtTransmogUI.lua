c21141eecc35db9e658472156798dd3cee91d520
Alan Baines <abaines.vt+github@gmail.com>
2024-11-30 21:15:17 -0500

    ExtTransmogUI 1.1.2
    ExtTransmogUI 1.1.2.zip
    
    https://www.curseforge.com/wow/addons/extended-transmog-ui

---
+++ ExtTransmogUI.lua
@@ -0,0 +1,33 @@
+function ExtTransmog_OnLoad(self)
+
+    ExtTransmog_RebuildFrame();
+
+end
+
+function ExtTransmog_RebuildFrame()
+
+    WardrobeFrame:SetWidth(1200);
+    WardrobeTransmogFrame:SetWidth(535);
+    WardrobeTransmogFrame.ModelScene:ClearAllPoints();
+    WardrobeTransmogFrame.ModelScene:SetPoint("TOP", WardrobeTransmogFrame, "TOP", 0, -4);
+    WardrobeTransmogFrame.ModelScene:SetWidth(420);
+    WardrobeTransmogFrame.ModelScene:SetHeight(420);
+    WardrobeTransmogFrame.Inset.BG:SetWidth(529);
+
+    WardrobeTransmogFrame.ModelScene.HeadButton:ClearAllPoints();
+    WardrobeTransmogFrame.ModelScene.HeadButton:SetPoint("TOP", WardrobeTransmogFrame.ModelScene, "TOP", -238, -41);
+    WardrobeTransmogFrame.ModelScene.HandsButton:ClearAllPoints();
+    WardrobeTransmogFrame.ModelScene.HandsButton:SetPoint("TOP", WardrobeTransmogFrame.ModelScene, "TOP", 235, -118);
+
+    WardrobeTransmogFrame.ModelScene.MainHandButton:ClearAllPoints();
+    WardrobeTransmogFrame.ModelScene.MainHandButton:SetPoint("TOP", WardrobeTransmogFrame.ModelScene, "BOTTOM", -26, -5);
+    WardrobeTransmogFrame.ModelScene.SecondaryHandButton:ClearAllPoints();
+    WardrobeTransmogFrame.ModelScene.SecondaryHandButton:SetPoint("TOP", WardrobeTransmogFrame.ModelScene, "BOTTOM", 27, -5);
+    WardrobeTransmogFrame.ModelScene.MainHandEnchantButton:ClearAllPoints();
+    WardrobeTransmogFrame.ModelScene.MainHandEnchantButton:SetPoint("BOTTOM", WardrobeTransmogFrame.ModelScene.MainHandButton, "BOTTOM", 0, -20);
+    WardrobeTransmogFrame.ModelScene.SecondaryHandEnchantButton:ClearAllPoints();
+    WardrobeTransmogFrame.ModelScene.SecondaryHandEnchantButton:SetPoint("BOTTOM", WardrobeTransmogFrame.ModelScene.SecondaryHandButton, "BOTTOM", 0, -20);
+    
+    UIPanelWindows["WardrobeFrame"].width = 1200;
+    
+end
